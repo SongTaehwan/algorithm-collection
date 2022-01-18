052a14fcc0fce04e65b9040ad1f8c197af9d4580
Song TaeHwan <axoghks@gmail.com>
2022-01-18 09:31:05 +0900

    Remove annotations

--- Can Place Flowers/Can_Place_Flowers.swift
+++ Can Place Flowers/Can_Place_Flowers.swift
@@ -9,9 +9,9 @@ import XCTest
 
 class Can_Place_Flowers: TestCase {
     func testCanPlaceFlowers() {
-//        XCTAssertEqual(sol.canPlaceFlowers([1,0,0,0,1], 1), true)
-//        XCTAssertEqual(sol.canPlaceFlowers([1,0,0,0,1], 2), false)
-//        XCTAssertEqual(sol.canPlaceFlowers([0,0,1,0,0], 1), true)
+        XCTAssertEqual(sol.canPlaceFlowers([1,0,0,0,1], 1), true)
+        XCTAssertEqual(sol.canPlaceFlowers([1,0,0,0,1], 2), false)
+        XCTAssertEqual(sol.canPlaceFlowers([0,0,1,0,0], 1), true)
         XCTAssertEqual(sol.canPlaceFlowers([0,0,0,0,0,1,0,0], 0), true)
     }
 
