fb9d33d48b7c4b7b2d4d81659e80614f299c6de2
Song TaeHwan <axoghks@gmail.com>
2022-01-19 17:50:41 +0900

    Add solution of 크래인 인형뽑기 게임

--- Programmers/Crane Puppet Draw Game/Crane_Puppet_Draw_Game.swift
+++ Programmers/Crane Puppet Draw Game/Crane_Puppet_Draw_Game.swift
@@ -10,7 +10,8 @@ import XCTest
 class Crane_Puppet_Draw_Game: TestCase {
 
     func testSolution() {
-        
+        XCTAssertEqual(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]), 4)
+        XCTAssertEqual(solution([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]], [1, 5, 3, 5, 1, 2, 1, 4]), 0)
     }
 
 }
--- Programmers/Crane Puppet Draw Game/Solution.swift
+++ Programmers/Crane Puppet Draw Game/Solution.swift
@@ -7,6 +7,53 @@
 
 import Foundation
 
+/// [Check out the problem](https://programmers.co.kr/learn/courses/30/lessons/64061)
 func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
-    return 0
+    let indice = moves.map { $0 - 1 }
+
+    var rows = Array(board.reversed())
+    var stack = [Int]()
+    
+    for index in indice {
+        let isEmptyRow = (rows.last ?? []).reduce(0,+) == 0
+
+        if isEmptyRow {
+            _ = rows.popLast()
+        }
+
+        var rowIndex = rows.count - 1
+
+        while rowIndex >= 0  {
+            let item = rows[rowIndex][index]
+
+            if item == 0 {
+                rowIndex -= 1
+                continue
+            }
+
+            stack.append(item)
+            rows[rowIndex][index] = 0
+            break
+        }
+    }
+    
+    var index = 0
+    var result = 0
+
+    while index < stack.count {
+        guard index + 1 != stack.count else { break }
+        
+        if stack[index] == stack[index + 1] {
+            stack.remove(at: index)
+            stack.remove(at: index)
+            result += 2
+            index = max(index - 1, 0)
+            
+            continue
+        }
+
+        index += 1
+    }
+    
+    return result
 }
