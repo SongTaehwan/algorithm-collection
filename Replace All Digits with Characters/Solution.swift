8794aa7b11b6a218b585dea870f90d4710ffa3a6
Song TaeHwan <axoghks@gmail.com>
2022-01-25 08:23:10 +0900

    Add solution of Replace All Digits with Characters

--- Replace All Digits with Characters/Solution.swift
+++ Replace All Digits with Characters/Solution.swift
@@ -10,6 +10,21 @@ import Foundation
 /// [Check out the problem](https://leetcode.com/problems/replace-all-digits-with-characters/)
 class Solution {
     func replaceDigits(_ s: String) -> String {
-        return ""
+        var result = ""
+        var asciiValue = UInt8(0)
+        
+        for (i, char) in s.enumerated() {
+            var value = String(char)
+            
+            if i % 2 == 0 {
+                asciiValue = char.asciiValue!
+            } else {
+                value = String(UnicodeScalar(asciiValue + UInt8(value)!))
+            }
+            
+            result += value
+        }
+        
+        return result
     }
 }
