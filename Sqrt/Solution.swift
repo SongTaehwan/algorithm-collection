b179b5d33b357d054a08575297b906ba2e4f2667
Song TaeHwan <axoghks@gmail.com>
2022-01-24 19:33:09 +0900

    Add Sqrt

---
+++ Sqrt/Solution.swift
@@ -0,0 +1,14 @@
+//
+//  Solution.swift
+//  Sqrt
+//
+//  Created by 송태환 on 2022/01/22.
+//
+
+import Foundation
+
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        
+    }
+}
---
+++ Sqrt/Sqrt.swift
@@ -0,0 +1,16 @@
+//
+//  Sqrt.swift
+//  Sqrt
+//
+//  Created by 송태환 on 2022/01/22.
+//
+
+import XCTest
+
+class Sqrt: TestCase {
+    func testSolution() {
+        XCTAssertEqual(sol.mySqrt(4), 2)
+        XCTAssertEqual(sol.mySqrt(8), 2)
+    }
+}
+
