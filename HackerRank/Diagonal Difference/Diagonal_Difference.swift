3bd5b96dd2e64bbe5803e50081d99d1ac8087b8e
Song TaeHwan <axoghks@gmail.com>
2022-01-26 11:43:35 +0900

    Add solution Diagonal Difference from HackerRank

---
+++ HackerRank/Diagonal Difference/Diagonal_Difference.swift
@@ -0,0 +1,19 @@
+//
+//  Diagonal_Difference.swift
+//  Diagonal Difference
+//
+//  Created by 송태환 on 2022/01/26.
+//
+
+import XCTest
+
+class Diagonal_Difference: XCTestCase {
+    func testSolution() {
+//        XCTAssertEqual(diagonalDifference(arr: [[1, 2, 3], [4, 5, 6], [9, 8, 9]]), 2)
+//        XCTAssertEqual(diagonalDifference(arr: [[11, 2, 4], [4, 5, 6], [10, 8, -12]]), 15)
+//        XCTAssertEqual(refactor(arr: [[1, 2, 3], [4, 5, 6], [9, 8, 9]]), 2)
+//        XCTAssertEqual(refactor(arr: [[11, 2, 4], [4, 5, 6], [10, 8, -12]]), 15)
+        XCTAssertEqual(refactor(arr: [[-1, 1, -7, -8], [-10, -8, -5, -2], [0, 9, 7, -1], [4, 4, -2, 1]]), 1)
+    }
+    
+}
---
+++ HackerRank/Diagonal Difference/Solution.swift
@@ -0,0 +1,45 @@
+//
+//  Solution.swift
+//  Diagonal Difference
+//
+//  Created by 송태환 on 2022/01/26.
+//
+
+import Foundation
+
+func diagonalDifference(arr: [[Int]]) -> Int {
+    var leftSum = 0
+    var rightSum = 0
+    let last = arr.count - 1
+    
+    for (i, row) in arr.enumerated() {
+        leftSum += row[i]
+        rightSum += row[last - i]
+    }
+    
+    let result = leftSum - rightSum
+    return result >= 0 ? result : ~result + 1
+}
+
+func refactor(arr: [[Int]]) -> Int {
+    let last = arr.count - 1
+    var result = 0
+    
+    for (i, row) in arr.enumerated() {
+        result += row[i]
+        result -= row[last-i]
+    }
+    
+    
+    return result >= 0 ? result : ~result + 1
+}
+
+func refactor2(arr: [[Int]]) -> Int {
+    let result = arr.enumerated().reduce(0) {
+        let left = $1.element[$1.offset]
+        let right = $1.element[arr.count - 1 - $1.offset]
+        return $0 + left - right
+    }
+    
+    return result >= 0 ? result : ~result + 1
+}
