0fbae82d7ecdc2640e299540b7c98714b6105afe
Song TaeHwan <axoghks@gmail.com>
2022-01-31 18:43:45 +0900

    Add Richest Customer Wealth

---
+++ Richest Customer Wealth/Richest_Customer_Wealth.swift
@@ -0,0 +1,14 @@
+//
+//  Richest_Customer_Wealth.swift
+//  Richest Customer Wealth
+//
+//  Created by 송태환 on 2022/01/31.
+//
+
+import XCTest
+
+class Richest_Customer_Wealth: TestCase {
+    override func testSolution() {
+        XCTAssertEqual(sol.maximumWealth([[1,2,3],[3,2,1]]), 6)
+    }
+}
---
+++ Richest Customer Wealth/Solution.swift
@@ -0,0 +1,15 @@
+//
+//  Solution.swift
+//  Richest Customer Wealth
+//
+//  Created by 송태환 on 2022/01/31.
+//
+
+import Foundation
+
+/// [Check out the problem](https://leetcode.com/problems/richest-customer-wealth/)
+class Solution {
+    func maximumWealth(_ accounts: [[Int]]) -> Int {
+        return 0
+    }
+}
