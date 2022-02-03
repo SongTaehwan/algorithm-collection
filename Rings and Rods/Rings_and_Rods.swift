6350eee8e7ef0dcf1f4bda544e783e951627c8d6
Song TaeHwan <axoghks@gmail.com>
2022-02-03 13:29:41 +0900

    Add solution of Rings and Rods

--- Rings and Rods/Rings_and_Rods.swift
+++ Rings and Rods/Rings_and_Rods.swift
@@ -13,6 +13,12 @@ class Rings_and_Rods: TestCase {
         XCTAssertEqual(sol.countPoints("B0B6G0R6R0R6G9"), 1)
         XCTAssertEqual(sol.countPoints("B0R0G0R9R0B0G0"), 1)
         XCTAssertEqual(sol.countPoints("G4"), 0)
+        XCTAssertEqual(sol.countPoints("G3R3R7B7R5B1G8G4B3G6"), 1)
+        
+        XCTAssertEqual(sol.refactor("R3G2B1"), 0)
+        XCTAssertEqual(sol.refactor("B0B6G0R6R0R6G9"), 1)
+        XCTAssertEqual(sol.refactor("B0R0G0R9R0B0G0"), 1)
+        XCTAssertEqual(sol.refactor("G4"), 0)
+        XCTAssertEqual(sol.refactor("G3R3R7B7R5B1G8G4B3G6"), 1)
     }
 }
-
--- Rings and Rods/Solution.swift
+++ Rings and Rods/Solution.swift
@@ -7,8 +7,38 @@
 
 import Foundation
 
+/// [Check out the problem](https://leetcode.com/problems/rings-and-rods/)
 class Solution {
     func countPoints(_ rings: String) -> Int {
-        return 0
+        var dict = [Character:Set<Character>]()
+        let chars = rings.map { $0 }
+        
+        for i in stride(from: 0, through: rings.count-1, by: 2) {
+            let color = chars[i]
+            let rod = chars[i+1]
+            
+            dict[rod, default: Set()].insert(color)
+        }
+        
+        return dict.filter { $0.value.count == 3 }.count
+    }
+    
+    func refactor(_ rings: String) -> Int {
+        let c = rings.count
+        var rods = [Int](repeating: 0, count: 10)
+        
+        for index in stride(from: 0, to: c, by: 2) {
+            let colorBit = rings[index] == "R" ? 1 : (rings[index] == "G" ? 2 : 4)
+            let rod: Int = rings[index + 1].wholeNumberValue!
+            rods[rod] = rods[rod] | colorBit
+        }
+        
+        return rods.reduce(into: 0) { $0 += $1 == 7 ? 1 : 0 }
+    }
+}
+
+extension String {
+    subscript(index: Int) -> Character {
+        return self[self.index(self.startIndex, offsetBy: index)]
     }
 }
