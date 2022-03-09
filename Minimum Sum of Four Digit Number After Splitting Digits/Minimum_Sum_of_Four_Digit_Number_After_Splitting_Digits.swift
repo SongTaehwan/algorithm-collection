774db55fe71325f3c129776cb06296a2513e2cd2
Song TaeHwan <axoghks@gmail.com>
2022-03-09 22:49:58 +0900

    Add Minimum Sum of Four Digit Number After Splitting Digits

---
+++ Minimum Sum of Four Digit Number After Splitting Digits/Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.swift
@@ -0,0 +1,16 @@
+//
+//  Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.swift
+//  Minimum Sum of Four Digit Number After Splitting Digits
+//
+//  Created by 송태환 on 2022/03/09.
+//
+
+import XCTest
+
+class Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits: TestCase {
+    override func testSolution() {
+        XCTAssertEqual(sol.minimumSum(2932), 52)
+        XCTAssertEqual(sol.minimumSum(4009), 13)
+    }
+}
+
---
+++ Minimum Sum of Four Digit Number After Splitting Digits/Solution.swift
@@ -0,0 +1,16 @@
+//
+//  Solution.swift
+//  Minimum Sum of Four Digit Number After Splitting Digits
+//
+//  Created by 송태환 on 2022/03/09.
+//
+
+import Foundation
+
+/// [Check out the problem](https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/)
+class Solution {
+    func minimumSum(_ num: Int) -> Int {
+        return 0
+    }
+}
+
