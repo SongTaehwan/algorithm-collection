af4d028195547c4403859b754ce53b7299f90a1f
Song TaeHwan <axoghks@gmail.com>
2022-04-05 18:50:58 +0900

    Add Counting Words With a Given Prefix

---
+++ Counting Words With a Given Prefix/Counting_Words_With_a_Given_Prefix.swift
@@ -0,0 +1,16 @@
+//
+//  Counting_Words_With_a_Given_Prefix.swift
+//  Counting Words With a Given Prefix
+//
+//  Created by 송태환 on 2022/04/05.
+//
+
+import XCTest
+
+class Counting_Words_With_a_Given_Prefix: TestCase {
+    override func testSolution() {
+        XCTAssertEqual(sol.prefixCount(["pay","attention","practice","attend"], "at"), 2)
+        XCTAssertEqual(sol.prefixCount(["leetcode","win","loops","success"], "code"), 2)
+    }
+}
+
---
+++ Counting Words With a Given Prefix/Solution.swift
@@ -0,0 +1,15 @@
+//
+//  Solution.swift
+//  Counting Words With a Given Prefix
+//
+//  Created by 송태환 on 2022/04/05.
+//
+
+import Foundation
+
+/// [Checkout the problem](https://leetcode.com/problems/counting-words-with-a-given-prefix/)
+class Solution {
+    func prefixCount(_ words: [String], _ pref: String) -> Int {
+        return 0
+    }
+}
