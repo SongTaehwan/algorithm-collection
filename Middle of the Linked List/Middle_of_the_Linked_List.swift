dba080e592b88d8adc3a60d177e4d14a4c9c8866
Song TaeHwan <axoghks@gmail.com>
2022-04-08 20:42:53 +0900

    Add Middle of the Linked List

---
+++ Middle of the Linked List/Middle_of_the_Linked_List.swift
@@ -0,0 +1,15 @@
+//
+//  Middle_of_the_Linked_List.swift
+//  Middle of the Linked List
+//
+//  Created by 송태환 on 2022/04/08.
+//
+
+import XCTest
+
+class Middle_of_the_Linked_List: TestCase {
+    override func testSolution() {
+//        XCTAssertEqual(<#T##expression1: Equatable##Equatable#>, <#T##expression2: Equatable##Equatable#>)
+    }
+}
+
---
+++ Middle of the Linked List/Solution.swift
@@ -0,0 +1,15 @@
+//
+//  Solution.swift
+//  Middle of the Linked List
+//
+//  Created by 송태환 on 2022/04/08.
+//
+
+import Foundation
+
+/// [Checkout the problem](https://leetcode.com/problems/middle-of-the-linked-list/)
+class Solution {
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        
+    }
+}
