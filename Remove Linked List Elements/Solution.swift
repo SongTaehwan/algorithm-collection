cf663e229c476e0a7c73541ca68e7f69b7fc7178
Song TaeHwan <axoghks@gmail.com>
2022-04-08 20:31:56 +0900

    Add solution of Remove Linked List Elements

--- Remove Linked List Elements/Solution.swift
+++ Remove Linked List Elements/Solution.swift
@@ -8,7 +8,32 @@
 import Foundation
 
 class Solution {
+    /// Recursive
+    /// Termination 상황에서 제일 마지막 노드에서 부터 Head 를 향해가면서 각 노드의 next 에 nil 또는 이전 노드 할당
+    func removeElements2(_ head: ListNode?, _ val: Int) -> ListNode? {
+        guard let head = head else { return nil }
+        head.next = removeElements(head.next, val)
+        return head.val == val ? head.next : head
+    }
+    
+    /// Iteration
     func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
-        return head
+        var newHead = head
+        
+        while newHead != nil && newHead?.val == val {
+            newHead = newHead?.next
+        }
+        
+        var node = newHead
+        
+        while node != nil {
+            if node?.next?.val == val {
+                node?.next = node?.next?.next
+            } else {
+                node = node?.next
+            }
+        }
+        
+        return newHead
     }
 }
