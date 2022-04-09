d57255c9237bf6f38c225be2c39ce9d2530e008d
Song TaeHwan <axoghks@gmail.com>
2022-04-09 19:42:43 +0900

    Add solution of Middle of the Linked List

--- Middle of the Linked List/Middle_of_the_Linked_List.swift
+++ Middle of the Linked List/Middle_of_the_Linked_List.swift
@@ -9,7 +9,23 @@ import XCTest
 
 class Middle_of_the_Linked_List: TestCase {
     override func testSolution() {
-//        XCTAssertEqual(<#T##expression1: Equatable##Equatable#>, <#T##expression2: Equatable##Equatable#>)
+        let list1 = createLinkedList([1,2,3,4,5])
+        let result1 = LinkedList(head: sol.middleNode(list1.head)!)
+        XCTAssertEqual(result1.getValues(), [3,4,5])
+        
+        let list2 = createLinkedList([1,2,3,4,5,6])
+        let result2 = LinkedList(head: sol.middleNode(list2.head)!)
+        XCTAssertEqual(result2.getValues(), [4,5,6])
+    }
+    
+    func testSolutionRefactored() {
+        let list1 = createLinkedList([1,2,3,4,5])
+        let result1 = LinkedList(head: sol.refactor(list1.head)!)
+        XCTAssertEqual(result1.getValues(), [3,4,5])
+        
+        let list2 = createLinkedList([1,2,3,4,5,6])
+        let result2 = LinkedList(head: sol.refactor(list2.head)!)
+        XCTAssertEqual(result2.getValues(), [4,5,6])
     }
 }
 
--- Middle of the Linked List/Solution.swift
+++ Middle of the Linked List/Solution.swift
@@ -10,6 +10,38 @@ import Foundation
 /// [Checkout the problem](https://leetcode.com/problems/middle-of-the-linked-list/)
 class Solution {
     func middleNode(_ head: ListNode?) -> ListNode? {
+        guard var node = head, head?.next != nil else { return head }
+        var nodes = [head]
         
+        while let next = node.next {
+            nodes.append(next)
+            node = next
+        }
+        
+        let middle = nodes.count / 2
+        
+        return nodes[middle]
+    }
+    
+    /// Two Pointer 중간 위치 찾는 알고리즘
+    /// pointer 1 - 1칸씩 이동, pointer 2 - 2칸씩 이동
+    //  pointer 1 / 2
+    //    1 / 1
+    //    2 / 3
+    //    3 / 5
+    //    4 / 7
+    //    5 / 9
+    //    6 / 11
+    //    7 / 13
+    func refactor(_ head: ListNode?) -> ListNode? {
+        var head = head
+        var temp = head
+        
+        while temp != nil && temp?.next != nil {
+            head = head?.next
+            temp = temp?.next?.next
+        }
+        
+        return head
     }
 }
