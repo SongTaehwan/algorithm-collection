535dfc4f67f03d7038da4cd4630cc49e14d1dddb
Song TaeHwan <axoghks@gmail.com>
2022-04-26 19:12:45 +0900

    Add Happy Number

---
+++ Happy Number/Happy_Number.swift
@@ -0,0 +1,16 @@
+//
+//  Happy_Number.swift
+//  Happy Number
+//
+//  Created by 송태환 on 2022/04/20.
+//
+
+import XCTest
+
+class Happy_Number: TestCase {
+    override func testSolution() {
+        XCTAssertEqual(sol.isHappy(19), true)
+//        XCTAssertEqual(sol.isHappy(2), false)
+    }
+}
+
