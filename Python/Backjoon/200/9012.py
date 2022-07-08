532009f0a64e040d642b1fb11430306b4833fe87
Song TaeHwan <axoghks@gmail.com>
2022-07-08 11:32:34 +0900

    feat: Add another solution of 9012

--- Python/Backjoon/200/9012.py
+++ Python/Backjoon/200/9012.py
@@ -1,20 +1,44 @@
 # https://www.acmicpc.net/problem/9012
 import sys
 
-for i in range(int(input())):
-    ps = sys.stdin.readline()
-    open = 0
+# for i in range(int(input())):
+#     ps = sys.stdin.readline()
+#     open = 0
+
+#     for char in ps:
+#         if char == "(":
+#             open += 1
+#         elif char == ")":
+#             open -= 1
+
+#         if open < 0:
+#             break
+
+#     if open != 0:
+#         print("NO")
+#     else:
+#         print("YES")
+
+# Stack 사용한 풀이
+
+
+def valid(ps):
+    stack = list()
 
     for char in ps:
         if char == "(":
-            open += 1
+            stack.append(char)
         elif char == ")":
-            open -= 1
+            if not stack:
+                return "NO"
 
-        if open < 0:
-            break
+            stack.pop()
 
-    if open != 0:
-        print("NO")
+    if stack:
+        return "NO"
     else:
-        print("YES")
+        return "YES"
+
+
+for i in range(int(input())):
+    print(valid(sys.stdin.readline()))
