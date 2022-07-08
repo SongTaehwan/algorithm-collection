6251daff096460cc01b7985b51b3cf4f908f768b
Song TaeHwan <axoghks@gmail.com>
2022-07-08 11:23:30 +0900

    feat: Add 9012

--- Python/Backjoon/200/9012.py
+++ Python/Backjoon/200/9012.py
@@ -1,1 +1,20 @@
 # https://www.acmicpc.net/problem/9012
+import sys
+
+for i in range(int(input())):
+    ps = sys.stdin.readline()
+    open = 0
+
+    for char in ps:
+        if char == "(":
+            open += 1
+        elif char == ")":
+            open -= 1
+
+        if open < 0:
+            break
+
+    if open != 0:
+        print("NO")
+    else:
+        print("YES")
