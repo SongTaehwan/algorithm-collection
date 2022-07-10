7a4cef2c45637ac4e3fe16f6e8607b42af932494
Song TaeHwan <axoghks@gmail.com>
2022-07-11 01:01:32 +0900

    feat: Add 1158

--- Python/Backjoon/200/1158.py
+++ Python/Backjoon/200/1158.py
@@ -1,1 +1,35 @@
 # https://www.acmicpc.net/problem/1158
+from collections import deque
+import sys
+
+count, nth = map(int, sys.stdin.readline().split())
+
+rest = deque(range(1, count + 1))
+dead = list()
+pointer = nth
+
+while rest:
+    target = rest.popleft()
+
+    if pointer == 1:
+        dead.append(str(target))
+        pointer = nth
+    else:
+        rest.append(target)
+        pointer -= 1
+
+print(f'<{", ".join(dead)}>')
+
+# Refactor
+count, nth = map(int, sys.stdin.readline().split())
+rest = list(range(1, count + 1))
+dead = list()
+
+index = 0
+
+while rest:
+  # 8 % 7 = 1 길이를 초과한 인덱스 값을 얻을 수 있음...😭
+    index = (index + nth - 1) % len(rest)
+    dead.append(str(rest.pop(index)))
+
+print(f'<{", ".join(dead)}>')
