10f99e3a2c03e4000df7b185a2ba7efb95b29c07
Song TaeHwan <axoghks@gmail.com>
2022-07-11 12:20:05 +0900

    feat: Add 1874

--- Python/Backjoon/200/1874.py
+++ Python/Backjoon/200/1874.py
@@ -1,1 +1,35 @@
 # https://www.acmicpc.net/problem/1874
+count = int(input())
+stack = list()
+result = list()
+last = 0
+
+while count:
+    number = int(input())
+
+    if number > last:
+        while number > last:
+            last += 1
+            stack.append(last)
+            result.append("+")
+
+        stack.pop()
+        result.append("-")
+    else:
+        found = False
+
+        if len(stack) != 0:
+            top = stack[-1]
+            stack.pop()
+            result.append("-")
+
+            if number == top:
+                found = True
+
+        if not found:
+            result = ["NO"]
+            break
+
+    count -= 1
+
+print(*result, sep="\n")
